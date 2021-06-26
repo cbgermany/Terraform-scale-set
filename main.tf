605d012e668905143d62751f64250543647080a7
Chris Germany <Chris.Germany@icloud.com>
2021-06-26 22:59:57 +0100

    additions

--- main.tf
+++ main.tf
@@ -69,88 +69,47 @@ resource "azurerm_lb_rule" "vmss" {
   probe_id                       = azurerm_lb_probe.vmss.id
 }
 
+# Create the Virtual Machine Scale Set
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                = var.name
+  resource_group_name = var.resource_group
+  location            = var.location
 
+  sku       = var.sku  
+  instances = 1
+  
+  admin_username = var.unix_admin
 
+  admin_ssh_key {
+    username   = var.unix_admin
+    public_key = var.public_key
+  }
 
-# # Create Network Security Group and rule
-# resource "azurerm_network_security_group" "mvssnsg" {
-#     name                = format("%s-%s", var.name, "vmnsg")
-#     location            = var.location
-#     resource_group_name = var.resource_group
-#     tags = {
-#         environment = "Terraform Demo"
-#     }
-# }
+  source_image_reference {
+    id = data.azurerm_image.id
+  }
 
-# # Allow port 22
-# resource "azurerm_network_security_rule" "mvssngs-ssh" {
-#   name                        = format("%s-%s", var.name, "nsgsshrule")
-#   priority                    = 1001
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = "22"
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = var.resource_group
-#   network_security_group_name = azurerm_network_security_group.mvssnsg.name
-# }
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
 
-# # Allow port 443
-# resource "azurerm_network_security_rule" "mvssngs-https" {
-#   name                        = format("%s-%s", var.name, "nsghttpsrule")
-#   priority                    = 1002
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "Tcp"
-#   source_port_range           = "*"
-#   destination_port_range      = "443"
-#   source_address_prefix       = "*"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = var.resource_group
-#   network_security_group_name = azurerm_network_security_group.mvssnsg.name
-# }
+  network_interface {
+    name    = "vmss_primary"
+    primary = true
 
-# resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
-#   name                = var.name
-#   resource_group_name = var.resource_group
-#   location            = var.location
-#   sku                 = var.sku
-#   instances           = 1
-#   admin_username      = var.unix_admin
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = var.subnet_id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.vmss.id]
+    }
+  }
 
-#   admin_ssh_key {
-#     username   = var.unix_admin
-#     public_key = var.public_key
-#   }
-
-#   source_image_reference {
-#     id = data.azurerm_image.id
-#   }
-
-#   os_disk {
-#     storage_account_type = "Standard_LRS"
-#     caching              = "ReadWrite"
-#   }
-
-#   network_interface {
-#     name    = "vmss_primary"
-#     primary = true
-
-#     ip_configuration {
-#       name      = "internal"
-#       primary   = true
-#       subnet_id = var.subnet_id
-#     }
-
-#  #   public_ip_address 
-#   }
-
-#   tags = merge(
-#     var.common_tags,
-#     tomap(
-#       {"Component" = "Database"}
-#     )
-#   )
-# }
+  tags = merge(
+    var.common_tags,
+    tomap(
+      {"Component" = "Database"}
+    )
+  )
+}
