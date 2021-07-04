c796b866eb3bdd4078e1fb552586ef4ff56ff748
Chris Germany <Chris.Germany@blueyonder.co.uk>
2021-07-04 08:44:21 +0000

    Correct tag

--- main.tf
+++ main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
   tags = merge(
     var.common_tags,
     tomap(
-      {"Component" = "Database"}
+      {"Component" = "Scale Set"}
     )
   )
 }
